565c47b7a39a46c12d910bbd21088740e34b160f
CockDeveloper <COCK.Developer@gmail.com>
2016-08-09 07:01:14 +0700

    * Create project:     + Run console win32     + Resolve exercise for string

---
+++ ExerciseString/ExerciseString.h
@@ -0,0 +1,13 @@
+// ExerciseString.h
+
+#pragma once
+
+using namespace System;
+
+namespace ExerciseString {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
